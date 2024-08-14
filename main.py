8e557bcb349f89d6f44e4c98dfa3beef8649f21b
Thomas Ott <43411998+tomott12345@users.noreply.github.com>
2024-08-14 09:31:56 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from fasthtml.common import *
+
+app,rt = fast_app()
+
+@rt('/')
+
+def get():
+    return Titled("Flood Forecast for Pompton River @ Route 23, Pompton Plains NJ", Img(src="static/forecast.png"), P("www.thomasott.io"))
+
+serve()
